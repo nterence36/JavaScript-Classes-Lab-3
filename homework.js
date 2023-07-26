4896bf9d261b9c0d01b4f90d6e68fde597178d4f
Terence Nkeng <nterence36@gmail.com>
2023-07-25 20:56:27 -0400

    created person class

--- homework.js
+++ homework.js
@@ -33,3 +33,60 @@ const hamster1 = new Hamster("I own", "Nick")
 hamster1.wheelRun()
 hamster1.eatFood()
 console.log(hamster1.getPrice())
+const hamster2 = new Hamster("Jocky", "Man")
+
+class Person {
+    constructor(name, age, height, weight, mood, hamsters, bankAccount){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+
+    getNmae(){
+        return this.name;
+    }
+
+    getAge(){
+        return this.age;
+    }
+
+    getWeight(){
+        return this.weight;
+    }
+
+    greet(){
+        console.log(`Hello ${this.name}`)
+    }
+
+    eat(){
+        this.weight++;
+        this.mood++;
+    }
+
+    exercise(){
+        return this.weight--
+    }
+
+    ageUp(){
+        
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount += 10
+    }
+
+    buyHamster(hamster) {
+        this.hamsters.push(hamster)
+        this.mood += 10;
+        this.bankAccount -= hamsters.getPrice();
+        
+    }
+
+}
+
+let Timmy = new Person("Tboy", 5);
